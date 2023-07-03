6c9f68b761385179d3a37ed766ca17a8a44c59da
brunocpradom <brunocpradom@gmail.com>
2023-07-03 13:41:19 -0300

    feat: test routes

---
+++ src/Firma/Controllers/EmpresaController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Firma.Managers;
+using Firma.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Firma.Controllers
+{
+    [ApiController]
+    [Route("empresa")]
+    public class EmpresaController
+    {
+        private IReceitaFederalService _rfService;
+        private ICsvParserService _csvParser;
+        private readonly ILogger<EmpresaController> _logger;
+
+        public EmpresaController(IReceitaFederalService rfService, ICsvParserService csvParser, ILogger<EmpresaController> logger)
+        {
+            _rfService = rfService;
+            _csvParser = csvParser;
+            _logger = logger;
+        }
+
+        [HttpGet("teste")]
+        public async Task<string> Get()
+        {
+            await _rfService.TaxRegimeDownload();
+            _logger.LogInformation("teste 123");
+            return "ok";
+        }
+
+        [HttpGet("csv")]
+        public string Csv()
+        {
+
+            return "ok";
+        }
+    }
+}
