ae1b1e761f551e4c87c67edd236c6551d7463132
brunocpradom <brunocpradom@gmail.com>
2023-07-10 17:41:57 -0300

    tests: create service fixture

--- tests/Firma.Tests.Integration/Fixtures/ServicesFixture.cs
+++ tests/Firma.Tests.Integration/Fixtures/ServicesFixture.cs
@@ -2,29 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Firma.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
 
 namespace Firma.Tests.Integration.Fixtures
 {
-    class ApplicationFactory : WebApplicationFactory<Program>
-    {
-        readonly Action<IWebHostBuilder> configure;
-
-        public ApplicationFactory(Action<IWebHostBuilder> configure) =>
-            this.configure = configure;
-
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            var dir = Directory.GetCurrentDirectory();
-            builder.UseContentRoot(dir);
-            configure(builder);
-        }
-    }
-
 
     public class ServicesFixture
     {
-        private protected ApplicationFactory WebApplicationFactory { get; private set; } = null!;
+        public string projectPath => Path.GetFullPath("../../../", Directory.GetCurrentDirectory());
+
+        public HttpClient httpClient = new();
+
+        public IConfiguration mockConfiguration => new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(projectPath, "appsettings.Development.json"))
+                .Build();
+
+        public ReceitaFederalClient receitaFederalClientMock()
+        {
+            var loggerRfClient = Mock.Of<ILogger<ReceitaFederalClient>>();
+            ReceitaFederalClient rfClient = new(loggerRfClient, httpClient, mockConfiguration);
+            return rfClient;
+        }
     }
 }
