4cc436d1f56d60804b9819ee7f538233c9013817
brunocpradom <brunocpradom@gmail.com>
2023-07-19 15:04:16 -0300

    fix: logger type

--- src/Firma/Managers/PartnersManager.cs
+++ src/Firma/Managers/PartnersManager.cs
@@ -17,11 +17,11 @@ namespace Firma.Managers
         private DataContext _context;
         private ICsvParserService _csvParser;
         private IReceitaFederalService _receitaFederal;
-        private ILogger<ReceitaFederalClient> _logger;
+        private ILogger<PartnersManager> _logger;
 
         public ManagerName Name => ManagerName.Partner;
 
-        public PartnersManager(DataContext context, ICsvParserService csvParser, IReceitaFederalService receitaFederal, ILogger<ReceitaFederalClient> logger)
+        public PartnersManager(DataContext context, ICsvParserService csvParser, IReceitaFederalService receitaFederal, ILogger<PartnersManager> logger)
         {
             _context = context;
             _csvParser = csvParser;
@@ -34,17 +34,18 @@ namespace Firma.Managers
             _logger.LogInformation("Creating Partner.");
             var company = await _context.Company.FirstAsync(c => c.BasicTaxId == record.BasicTaxId);
             var qualification = await _context.Qualification.FirstAsync(q => q.Code == record.Qualification);
-            var country = await _context.Country.FirstAsync(c => c.Code == record.Country);
+            var countryCode = string.IsNullOrWhiteSpace(record.Country) ? "105" : record.Country;
+            var country = await _context.Country.FirstAsync(c => c.Code == countryCode);
             var representativeQualification = await _context.Qualification.FirstAsync(q => q.Code == record.RepresentativeQualification);
             Partner partner = new()
             {
                 Company = company,
-                Identifier = (PartnerIdentifier)Enum.ToObject(typeof(PartnerIdentifier), record.Identifier),
+                Identifier = (PartnerIdentifier)Enum.ToObject(typeof(PartnerIdentifier), int.Parse(record.Identifier)),
                 Name = record.Name,
                 DocumentNumber = record.DocumentNumber,
                 Qualification = qualification,
-                AgeGroup = (AgeGroup)Enum.ToObject(typeof(AgeGroup), record.AgeGroup),
-                CompanyJoiningDate = record.CompanyJoiningDate,
+                AgeGroup = (AgeGroup)Enum.ToObject(typeof(AgeGroup), int.Parse(record.AgeGroup)),
+                CompanyJoiningDate = DateOnly.ParseExact(record.CompanyJoiningDate, "yyyyMMdd"),
                 Country = country,
                 LegalRepresentative = record.LegalRepresentative,
                 RepresentativeName = record.RepresentativeName,
